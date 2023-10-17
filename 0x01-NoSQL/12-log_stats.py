209969c0ab4984ed06fa6a038e5619b20da28567
SamZec <samzec4@gmail.com>
2023-10-17 06:12:05 +0000

    12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    12-log_stats.py
+    script that provides some stats about Nginx logs stored in MongoDB
+"""
+
+
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.logs.nginx
+
+print('{} logs'.format(db.estimated_document_count()))
+print('Methods:')
+print('\tmethod GET: {}'.format(db.count_documents({'method': 'GET'})))
+print('\tmethod POST: {}'.format(db.count_documents({'method': 'POST'})))
+print('\tmethod PUT: {}'.format(db.count_documents({'method': 'PUT'})))
+print('\tmethod PATCH: {}'.format(db.count_documents({'method': 'PATCH'})))
+print('\tmethod DELETE: {}'.format(db.count_documents({'method': 'DELETE'})))
+print('{} status check'.format(db.count_documents(
+      {'method': 'GET', 'path': '/status'})))
