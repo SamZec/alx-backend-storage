c6a0d3e53dd11509a54a0676ca69863e2d990be4
SamZec <samzec4@gmail.com>
2023-10-23 14:05:06 +0000

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,12 +2,44 @@
 """ exercise.py - module for the class Cache """
 
 
+import sys
+from functools import wraps
 import redis
 import uuid
 from typing import Union, Callable, Optional
 
 
 datatypes = Union[str, float, int, bytes]
+
+
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of the Cache class are called"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs for a particular function"""
+    key = method.__qualname__
+    _input = "".join([key, ":inputs"])
+    output = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper"""
+        self._redis.rpush(_input, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(output, str(res))
+        return res
+
+    return wrapper
 
 
 class Cache():
@@ -17,6 +49,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: datatypes) -> str:
         """store data in Redis using random key and return the key"""
         key = str(uuid.uuid4())
